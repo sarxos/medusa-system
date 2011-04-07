7c206d630afd6a7626c5793f359aad10cb6d57a7
Bartosz Firyn <songo.bercik@interia.pl>
2011-04-07 10:18:33 +0200

    Clear SmesX messages planned task

--- src/com/sarxos/medusa/cli/command/ClearSmesXMessagesCommand.java
+++ src/com/sarxos/medusa/cli/command/ClearSmesXMessagesCommand.java
@@ -6,24 +6,26 @@ import org.naturalcli.ICommandExecutor;
 import org.naturalcli.InvalidSyntaxException;
 import org.naturalcli.ParseResult;
 
+import com.sarxos.medusa.task.ClearSmesXMessagesTask;
+
 
 /**
  * Mark all SmesX messages as read.
  * 
  * @author Bartosz Firyn (SarXos)
  */
-public class ClearMessagesCommand extends Command implements ICommandExecutor {
+public class ClearSmesXMessagesCommand extends Command implements ICommandExecutor {
 
-	protected static final String syntax = "clear messages";
+	protected static final String syntax = "smesx clear";
 
 	protected static final String help = "Mark all SmesX messages as read";
 
-	public ClearMessagesCommand() throws InvalidSyntaxException {
+	public ClearSmesXMessagesCommand() throws InvalidSyntaxException {
 		prepare(syntax, help, this);
 	}
 
 	@Override
 	public void execute(ParseResult pr) throws ExecutionException {
-		// TODO
+		new ClearSmesXMessagesTask().run();
 	}
 }
