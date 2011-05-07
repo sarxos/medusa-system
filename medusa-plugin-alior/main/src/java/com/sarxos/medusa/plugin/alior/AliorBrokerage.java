b1b22b863b6c29148347fccf1d28880fa78659b8
Bartosz Firyn <sarxos@sarxos.pl>
2011-05-07 20:55:41 +0200

    Medusa Alior plugin

---
+++ medusa-plugin-alior/main/src/java/com/sarxos/medusa/plugin/alior/AliorBrokerage.java
@@ -0,0 +1,23 @@
+package com.sarxos.medusa.plugin.alior;
+
+import java.util.List;
+
+import com.sarxos.medusa.market.Account;
+import com.sarxos.medusa.market.Brokerage;
+import com.sarxos.medusa.market.Order;
+
+
+public class AliorBrokerage extends Brokerage {
+
+	@Override
+	public List<Account> getAccounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account getAccount(Order order) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
